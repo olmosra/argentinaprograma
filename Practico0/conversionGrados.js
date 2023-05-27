f6e1f44db704deb7d2fbd09dda8e5dc6e6ca5910
Roberto Olmos <olmosrobertoalejandro@gmail.com>
2023-05-27 09:41:44 -0300

    Agrego el archivo conversionGrados.js

---
+++ Practico0/conversionGrados.js
@@ -0,0 +1,8 @@
+f = 50;
+c = (f-32)*(5/9);
+console.log(c)
+console.log("Respuesta 3.a: 50 F son 10 C")
+f = 75;
+c = (f-32)*(5/9);
+console.log(c)
+console.log("Respuesta 3.b: 75 F son 23.88888888888889 C")
