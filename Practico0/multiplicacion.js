492e6aa45e5631a6d074545fba980c97c71cb961
Roberto Olmos <olmosrobertoalejandro@gmail.com>
2023-05-27 09:27:00 -0300

    agruegué resta y multiplicación del TP0

---
+++ Practico0/multiplicacion.js
@@ -0,0 +1,6 @@
+let x = 2;
+let y = 17;
+let z = 6;
+let multiplicacion = x*y*z
+console.log(multiplicacion)
+console.log("Respuesta 1.b: Si los numeros son 2*17*6 mi programa devuelve el valor "+(2*17*6))
---
+++ Practico0/resta.js
@@ -0,0 +1,5 @@
+let minuendo=10;
+let sustraendo=2;
+let resta= minuendo-sustraendo;
+console.log(resta)
+console.log("Respuesta 1.a: Si el minuendo es 10 y el sustraendo 2 mi programa devuelve el valor "+(10-2))
