5d649f5fd05098b8000d825eeda48bdb1d55a694
Roberto Olmos <olmosrobertoalejandro@gmail.com>
2023-05-27 09:43:34 -0300

    Practico N0 finalizado

---
+++ Practico0/areaRectangulo.js
@@ -0,0 +1,14 @@
+x = 5;
+y = 3;
+base = x;
+altura = y;
+area = base*altura;
+console.log(area) 
+console.log("Respuesta 2.a: El área de un rectángulo de base 5cm y altura 3cm es "+(5*3))
+x = 12.5;
+y= 5.3;
+base = x;
+altura = y;
+area = base*altura;
+console.log(area)
+console.log("Respuesta 2.b: El área de un rectángulo de base 12.5cm y altura 5.3cm es "+(12.5*5.3))
---
+++ Practico0/promedio3.js
@@ -0,0 +1,18 @@
+x = 5;
+y = 7;
+z = 9;
+promedio = (x+y+z)/3
+console.log(promedio)
+console.log("Respuesta 4.a: El promedio entre 5,7 y 9 es 7")
+x = -2;
+y = -3;
+z = -8;
+promedio = (x+y+z)/3
+console.log(promedio)
+console.log("Respuesta 4.b: El promedio entre -2,-3 y -8 es -4.333333333333333")
+x = -1.2;
+y = 5.3;
+z = -3.3;
+promedio = (x+y+z)/3
+console.log(promedio)
+console.log("Respuesta 4.c: El promedio entre -1.2, 5.3 y -3.3 es 0.2666666666666666")
