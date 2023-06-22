31558cac6b75451da6f76acd6dd9fa65039b92ef
Roberto Olmos <olmosrobertoalejandro@gmail.com>
2023-06-22 20:55:19 -0300

    Agrego ejercicio lista de invitados

---
+++ practico3/listaDeInvitados.js
@@ -0,0 +1,14 @@
+let personas = ["Jose", "Maria", "Juan", "Andres", "Lionel", "Sofia", "Leandro", "Emilia"];
+let admitidos = [];
+let rechazados = [];
+for ( let i = 0; i < personas.length; i++ ) {
+    if ( personas[i] === "Jose" || personas[i] === "Sofia") {
+rechazados.push(personas[i])
+    } else {
+        admitidos.push(personas[i])
+    }
+}
+console.log("La lista de invitados admitidos es: " + admitidos)
+console.log("La lista de invitados rechazados es: " + rechazados)
+console.log("La lista ordenada de invitados admitidos es: " + admitidos.sort())
+console.log("La lista ordenada de invitados rechazados es: " + rechazados.sort())
