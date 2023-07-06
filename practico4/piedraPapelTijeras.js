6083ec2987f243f8df177b2cff7e49c7ef4dc65f
Roberto Olmos <olmosrobertoalejandro@gmail.com>
2023-07-06 16:58:33 -0300

    esta andando

---
+++ practico4/piedraPapelTijeras.js
@@ -0,0 +1,65 @@
+const PIEDRA = "piedra";
+const PAPEL = "papel";
+const TIJERAS = "tijeras";
+
+function obtenerJugadaComputadora() {
+    const opciones = [PIEDRA, PAPEL, TIJERAS];
+    const indice = Math.floor(Math.random()*opciones.length );
+return opciones[indice];
+}
+
+function obtenerJugadaUsuario() {
+    const readlineSync = require('readline-sync');
+    let jugadaUsuario = readlineSync.question('Por favor ingrese su jugada "piedra", "papel" o "tijeras"').toLowerCase();
+   return jugadaUsuario;
+}
+
+function determinarGanador(jugadaComputador, jugadaUsuario) {
+    if (jugadaComputador === jugadaUsuario) {
+    return "Empate";
+    } else if (
+(jugadaComputador === PIEDRA && jugadaUsuario === TIJERAS) ||
+(jugadaComputador === TIJERAS && jugadaUsuario === PAPEL) ||
+(jugadaComputador === PAPEL && jugadaUsuario === PIEDRA )
+    ) { return "Gana la computadora";
+    } else {
+        return "Gana el usuario";
+    }
+}
+function jugarPiedraPapelTijeras() {
+    const readlineSync = require(`readline-sync`);
+    let jugadas = readlineSync.question("¿En cuantas jugadas quieres terminar el juego?");
+    let contadorComputadora = 0;
+    let contadorUsuario = 0;
+
+    for (let i = 1; i <= jugadas; i++) {
+        console.log(`Jugada ${i}:`);
+
+        const jugadaComputador = obtenerJugadaComputadora();
+        const jugadaUsuario = obtenerJugadaUsuario();
+
+        console.log(`La computadora eligió: ${jugadaComputador}`);
+        console.log(`El usuario eligió: ${jugadaUsuario}`);
+
+        const resultado = determinarGanador(jugadaComputador,jugadaUsuario);
+        console.log(`El resultado de la jugada ${i} es: ${resultado}`);
+
+        if (resultado === "Gana la computadora") {
+            contadorComputadora++;
+        } else if (resultado === "Gana el usuario") {
+            contadorUsuario++;
+        }
+    }
+    if (contadorComputadora > contadorUsuario) {
+        console.log("La computadora ha ganado el juego.");
+    } else if (contadorComputadora < contadorUsuario) {
+        console.log("El usuario ha ganado el juego.");
+    } else {
+        console.log("El juego ha terminado en empate.");
+    }
+   
+}
+jugarPiedraPapelTijeras();
+
+
+
